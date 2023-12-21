44653e08eb38c73b03c6228d88ee5a0778e3cacd
Kevin <koolkev246@gmail.com>
2023-12-20 22:38:03 -0500

    animations for fishing mostly complete

---
+++ Assets/Scripts/CharacterInputController.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterInputController : MonoBehaviour {
+
+    private float filteredForwardInput = 0f;
+    private float filteredTurnInput = 0f;
+
+    public bool InputMapToCircular = true;
+
+    public float forwardInputFilter = 5f;
+    public float turnInputFilter = 5f;
+
+    private float forwardSpeedLimit = 1f;
+
+
+    public float Forward
+    {
+        get;
+        private set;
+    }
+
+    public float Turn
+    {
+        get;
+        private set;
+    }
+
+    public bool Action
+    {
+        get;
+        private set;
+    }
+
+    public bool Jump
+    {
+        get;
+        private set;
+    }
+
+        
+
+	void Update () {
+		
+        //GetAxisRaw() so we can do filtering here instead of the InputManager
+        float h = Input.GetAxisRaw("Horizontal");// setup h variable as our horizontal input axis
+        float v = Input.GetAxisRaw("Vertical"); // setup v variables as our vertical input axis
+
+        if (InputMapToCircular)
+        {
+            // make coordinates circular
+            //based on http://mathproofs.blogspot.com/2005/07/mapping-square-to-circle.html
+            h = h * Mathf.Sqrt(1f - 0.5f * v * v);
+            v = v * Mathf.Sqrt(1f - 0.5f * h * h);
+        }
+
+        //do some filtering of our input as well as clamp to a speed limit
+        filteredForwardInput = Mathf.Clamp(Mathf.Lerp(filteredForwardInput, v, 
+            Time.deltaTime * forwardInputFilter), -forwardSpeedLimit, forwardSpeedLimit);
+
+        filteredTurnInput = Mathf.Lerp(filteredTurnInput, h, 
+            Time.deltaTime * turnInputFilter);
+
+        Forward = filteredForwardInput;
+        Turn = filteredTurnInput;
+
+        // //Capture "fire" button for action event
+        // Action = Input.GetButtonDown("Fire1");
+
+        // Jump = Input.GetButtonDown("Jump");
+	}
+}
--- Assets/Scripts/ThirdPController.cs
+++ Assets/Scripts/ThirdPController.cs
@@ -26,11 +26,30 @@ public class ThirdPController : MonoBehaviour
 
     public bool bhopEnabled = false;
 
+    // animator
+    public Animator animator;
+    private CharacterInputController cinput;
+    float _inputForward = 0f;
+    float _inputTurn = 0f;
+    public float speedChangeRate = 0.01f;
+    private float currentVelY = 0f;
+    private float targetVelY = 0f;
+    public bool alreadyCast = false;
+    public bool isFishing = false;
+    public bool isReeling = false;
+
+    // GameObject for fishing rod
+    public GameObject fishingRod;
+
     // Start is called before the first frame update
     void Start()
     {
-        controller = GetComponent<CharacterController>();
+        controller = GetComponent<CharacterController>();cinput = GetComponent<CharacterInputController>();
         Cursor.lockState = CursorLockMode.Locked; // Lock the cursor to the center of the screen.
+        cinput = GetComponent<CharacterInputController>();
+        animator.SetBool("startFishing", false);
+        // hide the fishing rod
+        fishingRod.SetActive(false);
     }
 
     // Update is called once per frame
@@ -40,61 +59,146 @@ public class ThirdPController : MonoBehaviour
         // We are using GetAxisRaw in case the player is using a controller.
         // Not tested yet.
         if (controller.isGrounded && playerVelocity.y < 0) {
+             animator.SetBool("isGrounded", true);
+            // animator.SetBool("isJumping", false);
+            // animator.SetBool("isFalling", false);
             playerVelocity.y = 0f;
+            playerVelocity.x = 0f;
+            playerVelocity.z = 0f;
         }
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
-
-        Vector3 direction = new Vector3(horizontal, 0, vertical).normalized;
-        if (direction.magnitude >= 0.1f) // if no input, stop applying movement
+        // Start fishing
+        if (Input.GetKeyDown(KeyCode.E))
         {
-            // Movement and also handles jumping while moving
-            // This if section might need to be moved to a new function and changed if bhop or other movement mechanics are added.
+            if (!isFishing)
+            {
+                animator.SetBool("startFishing", true);
+                isFishing = true;
+                fishingRod.SetActive(true);
+            }
+            // Stop fishing
+            else if (isFishing && !alreadyCast && !isReeling)
+            {
+                animator.SetBool("startFishing", false);
+                isFishing = false;
+                fishingRod.SetActive(false);
+            }
+        }
+
+        // Cast
+        else if (Input.GetMouseButtonDown(0) && isFishing && !alreadyCast && !isReeling)
+        {
+            animator.SetTrigger("cast");
+            alreadyCast = true;
+        }
+
+        // Reel in
+        if (Input.GetKeyDown(KeyCode.Q) && isFishing && alreadyCast && !isReeling)
+        {
+            animator.SetTrigger("reel");
+            isReeling = true;
+        }
+
+        if (isReeling)
+        {
+            AnimatorStateInfo stateInfo = animator.GetCurrentAnimatorStateInfo(0);
+            if (!stateInfo.IsName("Reel In")) {
+                alreadyCast = false;
+                isReeling = false;
+            }
+        }
+        
+        if (!isFishing)
+        {
+            if (cinput.enabled)
+            {
+                _inputForward = cinput.Forward;
+                _inputTurn = cinput.Turn;
+            }
+            float horizontal = Input.GetAxisRaw("Horizontal");
+            float vertical = Input.GetAxisRaw("Vertical");
+
+            var animState = animator.GetCurrentAnimatorStateInfo(0);
+            // animator.SetFloat("velX", _inputTurn);
+
+            // absolute value of _inputForward and _inputTurn
+            if (_inputForward < 0f)
+            {
+                _inputForward = -_inputForward;
+            }
+            if (_inputTurn < 0f)
+            {
+                _inputTurn = -_inputTurn;
+            }
+            targetVelY = Mathf.Max(_inputForward, _inputTurn);
+            Vector3 direction = new Vector3(horizontal, 0, vertical).normalized;
+            if (direction.magnitude >= 0.1f) // if no input, stop applying movement
+            {
+                // Movement and also handles jumping while moving
+                // This if section might need to be moved to a new function and changed if bhop or other movement mechanics are added.
 
 
-            // Calculate the angle between the player's input direction and the positive x-axis.
-            // This angle is then used to rotate the player's game object so that it faces the direction of movement.
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + camera.eulerAngles.y;
-            // Smoothly rotate the player's game object to face the direction of movement.
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothingVelocity, turnSmoothingTime);
-            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+                // Calculate the angle between the player's input direction and the positive x-axis.
+                // This angle is then used to rotate the player's game object so that it faces the direction of movement.
+                float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + camera.eulerAngles.y;
+                // Smoothly rotate the player's game object to face the direction of movement.
+                float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothingVelocity, turnSmoothingTime);
+                transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
-            Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+                Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
 
-            // if Jumping while moving
-            if (controller.isGrounded) {
-                if (bhopEnabled) {
-                    if (Input.GetButton("Jump")) {
-                        Jump();
+                // if Jumping while moving
+                if (controller.isGrounded)
+                {
+                    if (bhopEnabled)
+                    {
+                        if (Input.GetButton("Jump"))
+                        {
+                            Jump();
+                        }
                     }
-                } else {
-                    if (Input.GetButtonDown("Jump")) {
+                    else
+                    {
+                        if (Input.GetButtonDown("Jump"))
+                        {
+                            Jump();
+                        }
+                    }
+                    // player cannot "run" while in the air
+                    if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+                    {
+                        currentSpeed = runSpeed;
+                    }
+                    else
+                    {
+                        currentSpeed = speed;
+                    }
+                }
+                // current speed is preserved while in the air
+                controller.Move(moveDirection * currentSpeed * Time.deltaTime);
+            }
+            else if (controller.isGrounded)
+            {
+                // jumping in place
+                if (bhopEnabled)
+                {
+                    if (Input.GetButton("Jump"))
+                    {
                         Jump();
                     }
                 }
-                // player cannot "run" while in the air
-                if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift)) {
-                    currentSpeed = runSpeed;
-                } else {
-                    currentSpeed = speed;
+                else
+                {
+                    if (Input.GetButtonDown("Jump"))
+                    {
+                        Jump();
+                    }
                 }
             }
-            // current speed is preserved while in the air
-            controller.Move(moveDirection * currentSpeed * Time.deltaTime);
-        } else if (controller.isGrounded) {
-            // jumping in place
-                if (bhopEnabled) {
-                    if (Input.GetButton("Jump")) {
-                        Jump();
-                    }
-                } else {
-                    if (Input.GetButtonDown("Jump")) {
-                        Jump();
-                    }
-                }
-            }
-        playerVelocity.y += Physics.gravity.y * Time.deltaTime;
-        controller.Move(playerVelocity * Time.deltaTime);
+            currentVelY = Mathf.Lerp(currentVelY, targetVelY, speedChangeRate);
+            animator.SetFloat("velY", currentVelY);
+            playerVelocity.y += Physics.gravity.y * Time.deltaTime;
+            controller.Move(playerVelocity * Time.deltaTime);
+        }
     }
 
     // Character Controller handles collision detection different. Make sure to read docs.
